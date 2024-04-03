92f9ad581e33603abd795bb3d2fea9e4f0850f13
Zihad <huda.zihadul.23@gmail.com>
2024-04-03 14:58:05 +0600

    started the jobsite draft

---
+++ jobsite_project/php/db_connection.php
@@ -0,0 +1,14 @@
+ <?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$dsn = 'mysql:host=localhost;dbname=job_portal';
+$username = 'huda';
+$password = 'huda123';
+
+try {
+    $pdo = new PDO($dsn, $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e){
+    echo 'Database connection failed '. $e->getMessage(); 
+}
