7461d4a11908a4daf3c3570af655b28dc86cd519
Zihad <huda.zihadul.23@gmail.com>
2024-04-15 15:02:45 +0600

    created a new page for job posting

--- jobsite_project/php/db_connection.php
+++ jobsite_project/php/db_connection.php
@@ -2,7 +2,7 @@
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
-$dsn = 'mysql:host=localhost;dbname=job_portal';
+$dsn = 'mysql:host=localhost;dbname=jobsite';
 $username = 'huda';
 $password = 'huda123';
 
